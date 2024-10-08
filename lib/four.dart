16caf52b48d7830b163bd7863755b6dc2881bbdb
Slava <syavapk682@gmail.com>
2024-10-08 19:23:03 +0300

    four screen

--- lib/four.dart
+++ lib/four.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:test/four.dart';
 
-class Tree extends StatelessWidget {
-  const Tree({super.key});
+class Four extends StatelessWidget {
+  const Four({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: IconButton(
         onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => const Four()));
+        
         },
         icon: Icon(Icons.abc),
       ),
